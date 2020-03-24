ceed7d9d91fa67609c1e9e2e3f3e8fd6efe7d3aa
Mack McQuen <mackmcquen@comcast.net>
2020-03-24 22:35:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,14 +1,8 @@
-describe "mario" do
-  it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's-a me, Mario!"
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-  end
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
 end
 
-describe "all_phrases" do
-  it "takes in an argument and puts out the catch phrase" do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end
+def toadstool
+  puts status
 end
